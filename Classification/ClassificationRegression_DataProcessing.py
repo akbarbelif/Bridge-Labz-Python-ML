d5fc1cda7b9157099efe71cbc834ee13954687fe
akbarbelif@gmail.com <Lionking8055>
2019-06-17 18:23:29 +0530

    finding number of numeric and object type and checking it null and catelogical processing

--- Classification/ClassificationRegression_DataProcessing.py
+++ Classification/ClassificationRegression_DataProcessing.py
@@ -6,69 +6,97 @@ import pandas as pd
 import csv
 #import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
 
 
 #Data Processing
 class ClassificationRegressionModelPreparation:
 
-    def __int__(self,csv_path,Dep_Column,Indep_Column_Number=[],Indep_Column_Categorical=[]):
+    def data_processing(self,csv_path):
 
         #Read Csv file to dataset
-        dataset = pd.read_csv(self.csv_path)
+        dataset = pd.read_csv(csv_path)
         old_lenght = len(dataset)
         print("Length:\n", old_lenght)
-        print(dataset.head())
-        dataset=self.cat_encode(self,dataset,Indep_Column_Categorical)
-        new_lenght=len(dataset)
-        print("New Lenght: ",new_lenght)
+        #dataset_header=pd.DataFrame(dataset.head(n=0))
 
-        x_num_index = []
-        x_cat_index = []
-        X = pd.DataFrame()
-        y_index=dataset.columns.get_loc(Dep_Column)
-        Y=dataset.iloc[:,y_index: y_index + 1]
+        #if dataset.isnull().sum().sum() > 0:
+        print("DataFrame has Null Value.")
 
+        for i in dataset.columns:
+            if dataset[i].isnull().sum().sum() > 0:
 
-        #Numerical Column
-        if len(Indep_Column_Number) > 0:
-            for i in Indep_Column_Categorical:
-                x_num_index.append(dataset.columns.get_loc(Indep_Column_Number[i]))
-            X=dataset.iloc[:,x_num_index:x_num_index+1]
+                if (dataset[i].dtype == np.number):
+                    col_index=dataset.columns.get_loc(i)
+                    self.handle_null(col_index,"mean")
+                else:
+                    col_index = dataset.columns.get_loc(i)
+                    self.handle_null(col_index,"most_frequency")
 
-        #Categorocal Column
-        if len(Indep_Column_Categorical) > 0:
-            if len(Indep_Column_Number) > 0:
-                X=pd.concat(X,dataset.iloc[:,x_num_index:x_index +1],axis=1)
-            else:
-                for i in Indep_Column_Categorical:
-                    x_cat_index.append(dataset.column.get_loc(Indep_Column_Categorical[i]))
-                    X=dataset.iloc[:,x_cat_index:x_cat_index+1]
+                    #obj_index=dataset.columns.getloc(i)
+                    #print("Index of", i, " is ", obj_index)
+                    #self.handle_null(obj_index,strategy)
 
+        print("Completed Successfully")
+        #  print("Object type\n",dataset.select_dtypes(include=[np.object]))
+        # dep_column = input("Enter Dependence Column")
+        # categorical_column = input("Enter Categorical")
+        #
+        # for i in dataset_header:
+        #     if  i in categorical_column:
+        #
+        #         dataset=self.cat_encode(self,dataset,categorical_column)
+        #         new_lenght=len(dataset)
+        #         print("New Lenght: ",new_lenght)
+        #
+        # if len(dep_column) > 0:
+        #     if  dep_column in dataset_header:
+        #        dataset=self.handle_null(dep_column)
+        # else:
+        #     print("Dependent variable is must")
 
-
-        if len(y_index) > 0:
-           dataset=self.handle_null(y_index)
-
-        if len(Indep_Column_Number) > 0:
-            dataset=self.handle_null(x_index)
-
-        if len(X_Categorical_index) > 0:
-            dataset=self.stand_scaler(X_Categorical_index)
-
-        self.Storing_data(self,dataset)
+        # x_num_index = []
+        # x_cat_index = []
+        # X = pd.DataFrame()
+        # y_index=dataset.columns.get_loc(Dep_Column)
+        # Y=dataset.iloc[:,y_index: y_index + 1]
+        #
+        #
+        # #Numerical Column
+        # if len(Indep_Column_Number) > 0:
+        #     for i in Indep_Column_Categorical:
+        #         x_num_index.append(dataset.columns.get_loc(Indep_Column_Number[i]))
+        #     X=dataset.iloc[:,x_num_index:x_num_index+1]
+        #
+        # #Categorocal Column
+        # if len(Indep_Column_Categorical) > 0:
+        #     if len(Indep_Column_Number) > 0:
+        #         X=pd.concat(X,dataset.iloc[:,x_num_index:x_index +1],axis=1)
+        #     else:
+        #         for i in Indep_Column_Categorical:
+        #             x_cat_index.append(dataset.column.get_loc(Indep_Column_Categorical[i]))
+        #             X=dataset.iloc[:,x_cat_index:x_cat_index+1]
+        #
+        #
+        #
+        # if len(y_index) > 0:
+        #    dataset=self.handle_null(y_index)
+        #
+        # if len(Indep_Column_Number) > 0:
+        #     dataset=self.handle_null(x_index)
+        #
+        # if len(X_Categorical_index) > 0:
+        #     dataset=self.stand_scaler(X_Categorical_index)
+        #
+        # self.Storing_data(self,dataset)
 
     # Handle Missing Data
-    @staticmethod
-    def handle_null(self,column=[]):
 
-        if self.dataset.isnull().sum().sum() > 0:
-            from sklearn.impute import SimpleImputer
-            # set strategy to mean/median/most-Frequent
-            print("Check for NUll Value in Independent Variable")
-            with SimpleImputer(missing_values=np.nan,strategy='mean') as nan:
-                new_column=nan.fit_transform(column)
-            #impute = SimpleImputer(missing_values=np.nan, strategy='mean')
-            #new_column = impute.fit_transform(column)
+    def handle_null(self,col_index,strategy):
+
+            with SimpleImputer(missing_values=np.nan,strategy=strategy) as nan:
+                new_column=nan.fit_transform(col_index)
+
             print("Null Resolved\n:", new_column)
             return new_column
 
--- Classification/Support_Vector_Machine/Social_nw_ads_SCM.py
+++ Classification/Support_Vector_Machine/Social_nw_ads_SCM.py
@@ -9,46 +9,52 @@ from Classification.Classifier_LoadingnUnloadingModel import *
 
 class SCM:
 
-    def __int__(self,input_data_path,train_data_path):
+    try:
+        def scm_algo(self,input_data_path,train_data_path):
 
-        try:
+            #self.input_data_path=input_data_path
+            #self.train_data_path=train_data_path
 
-            ClassificationRegressionModelPreparation.Storing_data(self.input_data_path)
 
-            dataset=pd.read_csv(self.train_data_path)
-            print(dataset.head())
-            new_lenght = len(dataset)
-            print("New Length:\n", new_lenght)
-            X_index1=dataset.columns.get_loc("Age")
-            X_index2=dataset.columns.get_loc("EstimatedSalary")
-            print(X_index1)
-            X=dataset.iloc[:,[X_index1,X_index2]].values
-            y=dataset.iloc[:,4].values
-            # print("X Axis\n",X)
-            #print("Y Axis \n",y)
 
-            ##Splitting model into Training and testing
-            from sklearn.model_selection import train_test_split
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-            #print("Below Scaling",X_train)
+            ClassificationRegressionModelPreparation.data_processing(self,input_data_path)
 
-            #Standard Scaling
-            X_train=ClassificationRegressionModelPreparation.stand_scaler(X_train)
-            #print("after scaling:\n",X_train)
+            # dataset=pd.read_csv(self.train_data_path)
+            # print(dataset.head())
+            # new_lenght = len(dataset)
+            # print("New Length:\n", new_lenght)
+            # X_index1=dataset.columns.get_loc("Age")
+            # X_index2=dataset.columns.get_loc("EstimatedSalary")
+            # print(X_index1)
+            # X=dataset.iloc[:,[X_index1,X_index2]].values
+            # y=dataset.iloc[:,4].values
+            # # print("X Axis\n",X)
+            # #print("Y Axis \n",y)
+            #
+            # ##Splitting model into Training and testing
+            # from sklearn.model_selection import train_test_split
+            # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+            # #print("Below Scaling",X_train)
+            #
+            # #Standard Scaling
+            # X_train=ClassificationRegressionModelPreparation.stand_scaler(X_train)
+            # #print("after scaling:\n",X_train)
+            #
+            # y_pred=ClassificationRegression_Model.SCM_classifier(X_train,y_train,X_test,y_test)
+            # print("\nY_predicted Value:\n",y_pred)
+            # print("\nY_Ground True Value:\n", y_test)
+            #
+            # # Accuracy Calculation using confusion_matrix score
+            # Accuracy = ClassificationRegression_Model.confusion_matix(y_test, y_pred)
+            # print("\nAccuary Between True value and Predicted Value:\n", Accuracy)
+            # pass
 
-            y_pred=ClassificationRegression_Model.SCM_classifier(X_train,y_train,X_test,y_test)
-            print("\nY_predicted Value:\n",y_pred)
-            print("\nY_Ground True Value:\n", y_test)
+    except Exception  as e:
+        print (e)
+        pass
 
-            # Accuracy Calculation using confusion_matrix score
-            Accuracy = ClassificationRegression_Model.confusion_matix(y_test, y_pred)
-            print("\nAccuary Between True value and Predicted Value:\n", Accuracy)
-            pass
 
-        except Exception  as e:
-            print (e)
-            pass
-
-input_path="/home/admin1/Desktop/Deep-Learing/Classification/Random_forest_Classification/Data"
+input_path="../Input_data/Social_Network_Ads.csv"
 training_path="../Support_Vector_Machine/Data/training.csv"
-SCM(input_path,training_path)
+objscm=SCM()
+objscm.scm_algo(input_path,training_path)
