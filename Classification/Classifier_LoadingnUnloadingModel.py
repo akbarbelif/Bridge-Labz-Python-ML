73f1ee26158c6bd347080d6af4660d9c2bbd1214
akbarbelif@gmail.com <Lionking8055>
2019-06-18 14:51:06 +0530

    Adding loading and unloading Model for seralize and deserealize Model

---
+++ Classification/Classifier_LoadingnUnloadingModel.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import pickle
+
+
+class Classifier_Serialize_Deserialize_model:
+
+    def serializing_Model(self,classifier,filepath):
+
+        classifier_model=self.filepath+"/classifier_model.pkl"
+        # Save and Write the model to the pickle file
+        with open(self.classifier_model,'wb') as w:
+           pickle.dump(self.classifier_model,w,pickle.HIGHEST_PROTOCOL)
+           #Close the pickle Model instance
+           classifier_model.close()
+
+
+    def deSerializing_Model(self,classifier,filepath,testmodel):
+
+        testX, testY = self.testmodel
+        #Open and load by reading the pickle model
+        with open(self.classifier_model,'rb') as r:
+            load_model=pickle.load(r)
+            accuracy=load_model.score(testX,testY)
+            print("Accuracy Result for Test Data :\n",accuracy)
+
